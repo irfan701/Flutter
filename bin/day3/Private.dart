f4479088f9c28ad49a00e0e98082b909f4601143
irfan701 <irfanhossain701@gmail.com>
2022-06-16 01:47:49 +0400

    setter getter private variable
    setter getter private variable

---
+++ bin/day3/Private.dart
@@ -0,0 +1,9 @@
+import 'PrivateTwo.dart';
+
+import 'PrivateTwo.dart';
+
+void main() {
+  var obj = PrivateTwo();
+  obj.setData = 21;
+  print(obj.getData);
+}
---
+++ bin/day3/PrivateTwo.dart
@@ -0,0 +1,11 @@
+class PrivateTwo {
+  int? _age;
+
+  set setData(int age) {
+    _age = age;
+  }
+
+  get getData {
+    return _age;
+  }
+}
