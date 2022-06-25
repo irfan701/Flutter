8f1adc7a11d738665e611e39f9e89adf72e7e974
irfan701 <irfanhossain701@gmail.com>
2022-06-25 11:44:01 +0400

    ListTile
    ListTile

--- bin/day4/first_app/lib/pages/add.dart
+++ bin/day4/first_app/lib/pages/add.dart
@@ -5,6 +5,23 @@ class AddScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(backgroundColor: Colors.green);
+    return Scaffold(
+        //backgroundColor: Colors.green
+
+        body: SafeArea(
+      child: Column(
+        children: [
+          Card(
+            elevation: 5,
+            child: ListTile(
+              leading: CircleAvatar(),
+              title: Text("This is title"),
+              subtitle: Text("This is Subtitle"),
+              trailing: Text("\$ 250"),
+            ),
+          )
+        ],
+      ),
+    ));
   }
 }
