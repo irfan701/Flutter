1939396fbf14d89154695dfa51c0ff7ed8d68be8
irfan701 <irfanhossain701@gmail.com>
2022-06-29 14:07:46 +0400

    AnimatedContainer
    AnimatedContainer

--- bin/day8/day_eight/lib/pages/CodeAnimationScreen.dart
+++ bin/day8/day_eight/lib/pages/CodeAnimationScreen.dart
@@ -1,12 +1,43 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/foundation/key.dart';
-import 'package:flutter/src/widgets/framework.dart';
 
-class CodeAnimationScreen extends StatelessWidget {
-  const CodeAnimationScreen({Key? key}) : super(key: key);
+class CodeAnimationScreen extends StatefulWidget {
+  @override
+  State<CodeAnimationScreen> createState() => _CodeAnimationScreenState();
+}
+
+class _CodeAnimationScreenState extends State<CodeAnimationScreen> {
+  double _height = 100;
+
+  double _width = 200;
+
+  Color _color = Colors.green;
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+            AnimatedContainer(
+              duration: Duration(seconds: 3),
+              curve: Curves.bounceInOut,
+              height: _height,
+              width: _width,
+              decoration: BoxDecoration(
+                color: _color,
+                borderRadius: BorderRadius.all(Radius.circular(10)),
+              ),
+            ),
+            ElevatedButton(
+                onPressed: () {
+                  setState(() {
+                    _height = 300;
+                  });
+                },
+                child: Text("Animate"))
+          ],
+        ),
+      ),
+    );
   }
 }
