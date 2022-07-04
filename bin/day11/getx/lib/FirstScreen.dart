3d242ff25d13f411221ddc1eae634b525974ef6a
irfan701 <irfanhossain701@gmail.com>
2022-07-05 01:21:56 +0400

    Flutter MaterialPageRoute
    Flutter MaterialPageRoute

---
+++ bin/day11/getx/lib/FirstScreen.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:getx/SecondScreen.dart';
+
+class FirstScreen extends StatelessWidget {
+  const FirstScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Colors.green,
+        body: Column(
+          children: [
+            TextButton(
+                onPressed: () => Navigator.push(
+                    context, MaterialPageRoute(builder: (_) => SecondScreen())),
+                child: Text("Go To Second Screen"))
+          ],
+        ),
+      ),
+    );
+  }
+}
